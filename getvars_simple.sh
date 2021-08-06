f309d001c9bfd7bd1d7e6ad292e5053d3a0ddec5
Philip Wonski <philip318@gmail.com>
2021-08-06 16:45:15 -0400

    issues with restart..

--- getvars_simple.sh
+++ getvars_simple.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # create variables tiddler file
 echo "line 1" >> cligreetings.txt;
+
+
+hooks:
+  deploy: |
+    echo "title: build_greetings" >> /wiki4/tiddlers/build_greetings.tid
+    echo "text: $(PLATFORM_VARIABLES)" >> /wiki4/tiddlers/build_greetings.tid
