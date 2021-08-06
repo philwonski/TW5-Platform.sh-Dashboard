309eb4ddf1662c55362924f21a13ad42c3b24c7d
Philip Wonski <philip318@gmail.com>
2021-08-06 10:38:43 -0400

    added js macro to find platform env vars

---
+++ get_vars.js
@@ -0,0 +1,44 @@
+/*\
+title: $:/sandbox/myjax.js
+type: application/javascript
+module-type: macro
+
+
+<<getPlatform "tiddlername" "evar">>
+Example to get data from wikipedia: <<getPlatform "projectid" "PLATFORM_PROJECT">>
+
+
+\*/
+
+(function(){
+
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+    const { env } = process;
+
+    exports.name = "getPlatform";
+    
+    exports.params = [ {name: "tiddlername"},{name: "evar"}];
+    
+    
+    
+    /*
+    Run the macro
+    */
+    exports.run = function(tiddlername,evar) {
+    
+     var evar = evar || "None";
+    
+     var tiddlername = tiddlername || "None";
+    
+     let projectId = env.PLATFORM_PROJECT;
+
+      $tw.wiki.addTiddler({title:tiddlername, text:projectId, type: "application/json"});
+      console.log(projectId);
+      return "[["+tiddlername+"]]";
+    };
+    
+    
+    })();
